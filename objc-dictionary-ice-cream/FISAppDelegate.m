44ad0db01f968420cf94b5433c47b9b5cf0932b2
sean-wantwave <sean@wantwave.com>
2015-09-03 14:53:51 -0700

    Added methods for ice cream dictionaries

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -20,10 +20,49 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSDictionary *iceCreamPreferences = @{
+                                          @"Joe" : @"Peanut Butter and Chocolate",
+                                          @"Tim" : @"Natural Vanilla",
+                                          @"Sophie" : @"Mexican Chocolate",
+                                          @"Deniz" : @"Natural Vanilla",
+                                          @"Tom" : @"Mexican Chocolate",
+                                          @"Jim" : @"Natural Vanilla",
+                                          @"Mark" : @"Cookies 'n Cream",
+                                          };
+    
+    NSMutableArray *namesOfPeopleForIceCreamFlavor = [[NSMutableArray alloc] init];
+    
+    for (NSString *key in iceCreamPreferences) {
+        if ([iceCreamPreferences[key] isEqualToString:iceCream]) {
+            [namesOfPeopleForIceCreamFlavor addObject:key];
+        }
+    }
+
+    
+    return namesOfPeopleForIceCreamFlavor;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    NSMutableDictionary *countsOfIceCreamByName = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *key in iceCreamByName) {
+        
+        if ([[countsOfIceCreamByName allKeys] containsObject:iceCreamByName[key]]) {
+            continue;
+        } else {
+            NSArray *iceCreamNameCount = [self namesForIceCream:iceCreamByName[key]];
+            NSNumber* iceCreamCount = [NSNumber numberWithLong:[iceCreamNameCount count]];
+            [countsOfIceCreamByName setObject:iceCreamCount forKey:iceCreamByName[key]];
+        }
+        
+        
+        
+    }
+    
+    return countsOfIceCreamByName;
+}
 
 @end
