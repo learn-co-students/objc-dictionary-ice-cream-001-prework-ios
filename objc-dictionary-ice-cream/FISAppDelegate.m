d2d084a5a9939e388c02e8e9b9d403515a17fa23
guangLess <bha.guang@gmail.com>
2015-09-08 15:39:03 -0400

    ice Cream counting...

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"%@",[self namesForIceCream:@"Mexican Chocolate"]);
+    
     return YES;
 }
 
@@ -25,5 +28,48 @@
  * Write your custom method bodies here.
  
  */
+
+
+-(NSArray *)namesForIceCream:(NSString *)iceCream{
+    
+    NSDictionary * markNote = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                @"Tim" : @"Natural Vanilla",
+                                @"Sophie" : @"Mexican Chocolate",
+                                @"Deniz" : @"Natural Vanilla",
+                                @"Tom" : @"Mexican Chocolate",
+                                @"Jim" : @"Natural Vanilla",
+                                @"Mark" : @"Cookies 'n Cream",
+                                };
+
+    
+    NSArray * names = [markNote allKeysForObject:iceCream];
+    NSLog(@"%@", [markNote allKeysForObject:iceCream]);
+    
+    [self countsOfIceCream:markNote];
+    
+    return names;
+}
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName{
+    
+    NSArray * iceName = [iceCreamByName allValues];
+    NSLog(@"%@",iceName);
+
+    NSSet * uniqueIceCream = [NSSet setWithArray:iceName];
+    NSArray * arrayU = [uniqueIceCream allObjects];
+    NSLog(@"%@ and %@ array",uniqueIceCream,arrayU);
+
+    
+    NSCountedSet * countedIceCreamName = [NSCountedSet setWithArray:iceName];
+    NSMutableDictionary * finalIceCream = [[NSMutableDictionary alloc]init];
+
+    for ( int i = 0 ; i < arrayU.count ; i++){
+        int x = [countedIceCreamName countForObject: arrayU[i]]; // not sure what I did wrong....
+        [finalIceCream setObject:[NSNumber numberWithInt:x] forKey: arrayU[i]];
+        NSLog(@"%i-ICE %@",x,arrayU[i]);
+    }
+    NSLog(@"Final ices are %@",finalIceCream);
+    return  finalIceCream;
+}
 
 @end
