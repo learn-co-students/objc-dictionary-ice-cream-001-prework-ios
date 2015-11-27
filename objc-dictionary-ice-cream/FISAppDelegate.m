67493804d0463596220fcb2355600d2e75a00a1a
dominicprescod <dominicprescod@gmail.com>
2015-11-27 00:30:02 -0500

    Dictionary Basic Practice

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,32 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *) namesForIceCream:(NSString *)iceCream{
+    NSArray *names = [[NSMutableArray alloc]init];
+    NSDictionary *nameByIceCream = @{ @"Joe"    : @"Peanut Butter and Chocolate",
+                                     @"Tim"    : @"Natural Vanilla"            ,
+                                     @"Sophie" : @"Mexican Chocolate"          ,
+                                     @"Deniz"  : @"Natural Vanilla"            ,
+                                     @"Tom"    : @"Mexican Chocolate"          ,
+                                     @"Jim"    : @"Natural Vanilla"            ,
+                                     @"Mark"   : @"Cookies 'n Cream"          };
+    names = [[nameByIceCream allKeysForObject:iceCream]mutableCopy];
+    return names;
+}
+
+- (NSDictionary* ) countsOfIceCream:(NSDictionary *)iceCreamByName {
+    NSMutableDictionary *iceCreamCount = [[NSMutableDictionary alloc]init];
+    
+iceCreamCount[@"Peanut Butter and Chocolate"] = @([[iceCreamByName allKeysForObject:@"Peanut Butter and Chocolate"]count]);
+    
+iceCreamCount[@"Natural Vanilla"] = @([[iceCreamByName allKeysForObject:@"Natural Vanilla"]count]);
+
+iceCreamCount[@"Mexican Chocolate"] = @([[iceCreamByName allKeysForObject:@"Mexican Chocolate"]count]);
+
+iceCreamCount[@"Cookies 'n Cream"] = @([[iceCreamByName allKeysForObject:@"Cookies 'n Cream"]count]);
+
+    
+    return iceCreamCount;
+}
 
 @end
