385587d4b1330dcec3a92c69c11566800e189cfd
asbhat <aditya.s.bhat@gmail.com>
2015-12-12 21:23:27 -0500

    ice cream dictionaries

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,38 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSMutableArray *names = [[NSMutableArray alloc] init];
+    
+    NSDictionary *favoriteIceCreams = @{ @"Joe"     : @"Peanut Butter and Chocolate",
+                                         @"Tim"     : @"Natural Vanilla",
+                                         @"Sophie"  : @"Mexican Chocolate",
+                                         @"Deniz"   : @"Natural Vanilla",
+                                         @"Tom"     : @"Mexican Chocolate",
+                                         @"Jim"     : @"Natural Vanilla",
+                                         @"Mark"    : @"Cookies 'n Cream"};
+    
+    for (NSString *name in [favoriteIceCreams allKeys]) {
+        if ([iceCream isEqualToString:favoriteIceCreams[name]]) {
+            [names addObject:name];
+        }
+    }
+    
+    return names;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    NSSet *allFlavors = [NSSet setWithArray:[iceCreamByName allValues]];
+    
+    NSMutableDictionary *iceCreamCounts = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *flavor in allFlavors) {
+        iceCreamCounts[flavor] = @([[self namesForIceCream:flavor] count]);
+    }
+    
+    return iceCreamCounts;
+}
 
 @end
