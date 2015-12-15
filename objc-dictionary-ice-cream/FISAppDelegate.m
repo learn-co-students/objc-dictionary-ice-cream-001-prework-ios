3136e7dc90007f92a2a80504e382883517b62599
Dnld <steinert.donald@gmail.com>
2015-12-15 11:21:23 -0500

    implements ice cream b name

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -16,4 +16,29 @@
  
  */
 
+- (NSArray *)namesForIceCream:(NSString *)iceCream
+{
+    NSDictionary *iceCreamByName = @{ @"Joe": @"Peanut Butter and Chocolate",
+                                      @"Tim": @"Natural Vanilla",
+                                      @"Sophie": @"Mexican Chocolate",
+                                      @"Deniz": @"Natural Vanilla",
+                                      @"Tom": @"Mexican Chocolate",
+                                      @"Jim": @"Natural Vanilla",
+                                      @"Mark": @"Cookies 'n Cream"};
+    NSMutableArray *names = [[NSMutableArray alloc] init];
+    
+    for (NSString *name in iceCreamByName) {
+        if ([iceCreamByName[name] isEqualToString:iceCream]) {
+            [names addObject:name];
+        }
+    }
+    
+    return names;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName
+{
+    return nil;
+}
+
 @end
