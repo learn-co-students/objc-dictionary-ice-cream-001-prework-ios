d1ad9cee6c0059a17a7904e40a52a5eb39a1cdcc
Shena Yoshida <sheyoshida@gmail.com>
2015-12-17 17:00:43 -0500

    Done.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -6,14 +6,46 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    NSString *testFlavor = @"Mexican Chocolate";
+    [self namesForIceCream:testFlavor];
+    
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSDictionary *iceCreamByName = @{
+                        @"Joe"    : @"Peanut Butter and Chocolate",
+                        @"Tim"    : @"Natural Vanilla"            ,
+                        @"Sophie" : @"Mexican Chocolate"          ,
+                        @"Deniz"  : @"Natural Vanilla"            ,
+                        @"Tom"    : @"Mexican Chocolate"          ,
+                        @"Jim"    : @"Natural Vanilla"            ,
+                        @"Mark"   : @"Cookies 'n Cream"
+                        };
+    
+    NSMutableArray *iceCreamPeople = [[NSMutableArray alloc] init];
+
+    for (NSString *key in iceCreamByName) {
+        if ([iceCreamByName[key] isEqualToString:iceCream]) {
+            [iceCreamPeople addObject:key];
+        }
+    }
+    NSLog(@"flavor: %@ people: %@", iceCream, iceCreamPeople); // test it!
+    return iceCreamPeople;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    NSMutableDictionary *inventory = [[NSMutableDictionary alloc] init];
+    NSSet *allFlavors = [NSSet setWithArray:[iceCreamByName allValues]];
+    
+    for (NSString *flavor in allFlavors) {
+        inventory[flavor] = @([[self namesForIceCream:flavor] count]);
+    }
+    
+    return inventory;
+}
 
 @end
