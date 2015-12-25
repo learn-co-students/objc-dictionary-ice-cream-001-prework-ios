4c5174dd555ad8a350695fadbc6896837a717b35
dmead28 <dmead28@gmail.com>
2015-12-24 23:17:10 -0500

    Complete: dmead

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,42 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+
+-(NSArray*)namesForIceCream:(NSString*)iceCream {
+    NSMutableArray *names = [[NSMutableArray alloc] init];
+    
+    NSDictionary *iceCreamByName = @{
+                                     @"Joe"     : @"Peanut Butter and Chocolate" ,
+                                     @"Tim"     : @"Natural Vanilla"             ,
+                                     @"Sophie"  : @"Mexican Chocolate"        ,
+                                     @"Deniz"   : @"Natural Vanilla"           ,
+                                     @"Tom"     : @"Mexican Chocolate"           ,
+                                     @"Jim"     : @"Natural Vanilla"             ,
+                                     @"Mark"    : @"Cookies 'n Cream"           };
+    
+    for (NSString *key in iceCreamByName) {
+        if ([iceCreamByName[key] isEqualToString:iceCream]) {
+            [names addObject:key];
+        }
+    }
+    
+    return names;
+}
+
+-(NSDictionary*)countsOfIceCream:(NSDictionary*)iceCreamByName {
+    NSMutableDictionary *countsByIceCream = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *name in iceCreamByName) {
+        NSString *flavor = iceCreamByName[name];
+        
+        if (countsByIceCream[flavor]) {
+            countsByIceCream[flavor] = @([countsByIceCream[flavor] integerValue] + 1);
+        } else {
+            countsByIceCream[flavor] = @1;
+        }
+    }
+    
+    return countsByIceCream;
+}
 
 @end
