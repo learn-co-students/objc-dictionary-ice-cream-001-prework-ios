2686fbd38967f81061ed0602f128493b168da2b8
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-05 16:13:31 -0500

    Adds NSDictionary preferredIceCreamFlavors to -[FISAppDelegate namesForIceCream:].

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -12,6 +12,13 @@
 
 - (NSArray *)namesForIceCream:(NSString *)iceCream {
     
+    NSDictionary *preferredIceCreamFlavors = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                               @"Tim" : @"Natural Vanilla",
+                                               @"Sophie" : @"Mexican Chocolate",
+                                               @"Deniz" : @"Natural Vanilla",
+                                               @"Tom" : @"Mexican Chocolate",
+                                               @"Jim" : @"Natural Vanilla",
+                                               @"Mark" : @"Cookies 'n Cream"};
     return nil;
 }
 
