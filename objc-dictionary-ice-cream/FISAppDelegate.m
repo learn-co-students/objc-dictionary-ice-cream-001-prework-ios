f679f0402972f9db99be4cd3aadcc8ba84d7c8b2
bclous <brian.clouser@gmail.com>
2016-01-14 13:44:18 -0500

    finished lab all tests passed

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -16,4 +16,48 @@
  
  */
 
+- (NSArray *)namesForIceCream:(NSString *)iceCream
+{
+    
+    NSDictionary *iceCreamByName = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                      @"Tim" : @"Natural Vanilla",
+                                      @"Sophie" : @"Mexican Chocolate",
+                                      @"Deniz" : @"Natural Vanilla",
+                                      @"Tom" : @"Mexican Chocolate",
+                                      @"Jim" : @"Natural Vanilla",
+                                      @"Mark" : @"Cookies 'n Cream"};
+    
+    NSMutableArray *peopleWhoWantThatIceCream = [[NSMutableArray alloc] init];
+    
+    for (NSString *key in iceCreamByName)
+    {
+        BOOL iceCreamMatch = [iceCreamByName[key] isEqualToString:iceCream];
+        
+        if(iceCreamMatch)
+        {
+            [peopleWhoWantThatIceCream addObject:key];
+        }
+    }
+    
+    return peopleWhoWantThatIceCream;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName
+{
+    
+    NSMutableDictionary *iceCreamsByNumber = [@{} mutableCopy];
+    
+    for (NSString *key in iceCreamByName)
+    {
+        if ( ![[iceCreamsByNumber allKeys] containsObject:iceCreamByName[key]])
+        {
+            NSUInteger numberOfPeopleWhoLikeThatIceCreamInt = [[self namesForIceCream:iceCreamByName[key]] count];
+            NSNumber *numberOfPeopleWhoLikeThatIceCream = @(numberOfPeopleWhoLikeThatIceCreamInt);
+            [iceCreamsByNumber setObject:numberOfPeopleWhoLikeThatIceCream forKey:iceCreamByName[key]];
+        }
+    }
+    
+    return iceCreamsByNumber;
+}
+
 @end
