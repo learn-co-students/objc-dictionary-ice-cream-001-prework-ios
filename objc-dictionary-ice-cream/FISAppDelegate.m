9efec118e91fa137e993010a5f39ce3374a73006
dochonglo <ismaelbarry777@hotmail.com>
2016-04-15 22:29:26 -0400

    Lab 16: Dictionary Basic Practice is complete.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -16,4 +16,44 @@
  
  */
 
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSMutableArray *namesOfPeople = [[NSMutableArray alloc] init];
+    NSDictionary *marksNotes = @{ @"Joe" : @"Peanut Butter and Chocolate",
+                                  @"Tim" : @"Natural Vanilla",
+                                  @"Sophie" : @"Mexican Chocolate",
+                                  @"Deniz" : @"Natural Vanilla",
+                                  @"Tom" : @"Mexican Chocolate",
+                                  @"Jim" : @"Natural Vanilla",
+                                  @"Mark" : @"Cookies 'n Cream"};
+    
+    for (NSString *keys in marksNotes) {
+        if ([marksNotes[keys] isEqualToString: iceCream]) {
+            [namesOfPeople addObject:keys];
+        }
+    }
+    return namesOfPeople;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+   
+    NSMutableDictionary *returnDictionary = [[NSMutableDictionary alloc] init];
+    NSArray *nameArray = [iceCreamByName allValues];
+    
+    for (NSString *nameIndex in nameArray) {
+        if (![returnDictionary objectForKey:nameIndex]) {
+            NSUInteger counterInt = 0;
+            for (NSString *name in iceCreamByName) {
+                if ([iceCreamByName[name] isEqualToString:nameIndex]) {
+                    counterInt++;
+                }
+            }
+            NSNumber *counterNumber = @(counterInt);
+            returnDictionary[nameIndex] = counterNumber;
+        }
+    }
+    return returnDictionary;
+}
+
 @end
+
