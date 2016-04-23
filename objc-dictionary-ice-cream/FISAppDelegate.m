fd03ba1cc25d95d83e268033b3ab1233640fbb44
dochonglo <ismaelbarry777@hotmail.com>
2016-04-23 17:43:49 -0400

    Added lab objectives and method descriptions (in header file) for clarification purposes.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -4,6 +4,10 @@
 
 @implementation FISAppDelegate
 
+// Objectives:
+// 1. Practice creating and using a dictionary.
+// 2. Write methods to interpret the information in a dictionary.
+// 3. Practice converting to a mutable dictionary and modifying it programmatically.
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -53,6 +57,19 @@
         }
     }
     return returnDictionary;
+    
+    
+    /*
+    // Solutions:
+    NSMutableDictionary *countsOfIceCream = [[NSMutableDictionary alloc] init];
+    for (NSString *key in iceCreamByName) {
+        NSString *iceCream = iceCreamByName[key];
+        NSArray *currentNames = [self namesForIceCream:iceCream];
+        countsOfIceCream[iceCream] = [NSNumber numberWithInteger:currentNames.count];
+    }
+    return [NSDictionary dictionaryWithDictionary:countsOfIceCream];
+    */
+    
 }
 
 @end
