1e7632e9247059a89da53919208083e564975036
chriswebb09 <chris.webb5249@gmail.com>
2016-05-08 14:02:35 -0400

    Done.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,35 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    NSMutableArray *iceCreamMutableArray = [[NSMutableArray alloc]init];
+    NSDictionary *namesAndIceCream = @{ @"Joe"    : @"Peanut Butter and Chocolate",
+                                        @"Tim"    : @"Natural Vanilla"            ,
+                                        @"Sophie" : @"Mexican Chocolate"          ,
+                                        @"Deniz"  : @"Natural Vanilla"            ,
+                                        @"Tom"    : @"Mexican Chocolate"          ,
+                                        @"Jim"    : @"Natural Vanilla"            ,
+                                        @"Mark"   : @"Cookies 'n Cream"          };
+    for (NSString *key in namesAndIceCream) {
+        if ([namesAndIceCream[key] isEqualToString:iceCream]) {
+            [iceCreamMutableArray addObject:key];
+        }
+    }
+    
+    return iceCreamMutableArray;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    NSMutableDictionary *countDictionary = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *key in iceCreamByName) {
+        NSString *iceCreamName = iceCreamByName[key];
+        NSArray *names = [self namesForIceCream:iceCreamByName[key]];
+        NSUInteger nameUnsignedIntNumbers = [names count];
+        NSNumber *nameNumbers = @(nameUnsignedIntNumbers);
+        [countDictionary setObject:nameNumbers forKey:iceCreamName];
+    }
+    return countDictionary;
+}
 
 @end
