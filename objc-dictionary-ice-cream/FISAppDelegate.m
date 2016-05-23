523b13e6a1be1a0eb0698afd7f871a883a74450b
ugowe <joe@spheares.com>
2016-05-23 01:09:19 -0400

    add code to lab Ice Cream

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,32 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream
+{
+      NSDictionary *iceCreamByName = @{ @"Joe"    : @"Peanut Butter and Chocolate",
+                                        @"Tim"    : @"Natural Vanilla"            ,
+                                        @"Sophie" : @"Mexican Chocolate"          ,
+                                        @"Deniz"  : @"Natural Vanilla"            ,
+                                        @"Tom"    : @"Mexican Chocolate"          ,
+                                        @"Jim"    : @"Natural Vanilla"            ,
+                                        @"Mark"   : @"Cookies 'n Cream"          };
+    
+    
+      return [iceCreamByName allKeysForObject:iceCream];
+    
+    }
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName
+{
+      NSMutableDictionary *countsOfIceCream = [[NSMutableDictionary alloc] init];
+      NSArray *iceCreams = [iceCreamByName allValues];
+      for (NSString *iceCream in iceCreams) {
+            // Check if this ice cream is already in the counts dictionary
+            if (![countsOfIceCream objectForKey:iceCream]) {
+                  [countsOfIceCream setObject:@([self namesForIceCream:iceCream].count) forKey:iceCream];
+                }
+          }
+      return countsOfIceCream;
+    }
 
 @end
