d3d61d2f11718a2cf76dad7df4f621d87a1f5d2a
Elli Scharlin <escharlin@wesleyan.edu>
2016-05-25 19:33:03 -0400

    Working on names for Ice Cream
    returning fan of the flavor by searching the dictionary for keys of
    flavor names.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -9,11 +9,40 @@
     // Override point for customization after application launch.
     return YES;
 }
-
-/**
- 
- * Write your custom method bodies here.
- 
- */
+-(NSArray *)namesForIceCream:(NSString *)iceCream{
+    NSMutableArray *fanOfTheFlavor = [[NSMutableArray alloc] init];
+    NSMutableDictionary *nameAndIceCream =
+  @{    @"Joe"      :   @"Peanut Butter and Chocolate"  ,
+        @"Tim"      :   @"Natural Vanilla"              ,
+        @"Sophie"   :   @"Mexican Chocolate"            ,
+        @"Deniz"    :   @"Natural Vanilla"              ,
+        @"Tom"      :   @"Mexican Chocolate"            ,
+        @"Jim"      :   @"Natural Vanilla"              ,
+        @"Mark"     :   @"Cookies 'n Cream"            };
+    
+    if ([ [nameAndIceCream allKeys] containsObject:@"Peanut Butter and Chocolate"]) {
+        [fanOfTheFlavor addObject: (@"%@", nameAndIceCream[@"Peanut Butter and Chocolate"])];
+         return fanOfTheFlavor;
+         }
+         
+    else if ([ [nameAndIceCream allKeys] containsObject:@"Mexican Chocolate"]) {
+        [fanOfTheFlavor addObject: (@"%@", nameAndIceCream[@"Mexican Chocolate"])];
+         return fanOfTheFlavor;
+         }
+    else if ([ [nameAndIceCream allKeys] containsObject:@"Natural Vanilla"]) {
+        [fanOfTheFlavor addObject: (@"%@", nameAndIceCream[@"Natural Vanilla"])];
+        return fanOfTheFlavor;
+    }
+    else if ([ [nameAndIceCream allKeys] containsObject:@"Cookies 'n Cream"]) {
+        [fanOfTheFlavor addObject: (@"%@", nameAndIceCream[@"Cookies 'n Cream"])];
+        return fanOfTheFlavor;
+    }
+    return nil;
+}
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName{
+    
+    
+    return nil;
+}
 
 @end
