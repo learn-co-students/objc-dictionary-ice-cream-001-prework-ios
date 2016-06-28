13088261b4e31434788dd22f0161e71b1990cc7b
William Robinson <wsrobins@uvm.edu>
2016-06-28 15:10:00 -0400

    Ice cream dictionary practice // lab complete.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -6,14 +6,56 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    NSDictionary *namesAndIceCreamPref = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                           @"Tim" : @"Natural Vanilla",
+                                           @"Sophie" : @"Mexican Chocolate",
+                                           @"Deniz" : @"Natural Vanilla",
+                                           @"Tom" : @"Mexican Chocolate",
+                                           @"Jim" : @"Natural Vanilla",
+                                           @"Mark" : @"Cookies 'n Cream"};
+    [self countsOfIceCream:namesAndIceCreamPref];
+    
+    
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSDictionary *namesAndIceCreamPref = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                           @"Tim" : @"Natural Vanilla",
+                                           @"Sophie" : @"Mexican Chocolate",
+                                           @"Deniz" : @"Natural Vanilla",
+                                           @"Tom" : @"Mexican Chocolate",
+                                           @"Jim" : @"Natural Vanilla",
+                                           @"Mark" : @"Cookies 'n Cream"};
+    
+    NSMutableArray *namesWithPref = [[NSMutableArray alloc] init];
+    
+    for (NSString *name in namesAndIceCreamPref) {
+        
+        if ([namesAndIceCreamPref[name] isEqualToString:iceCream]) {
+            
+            [namesWithPref addObject:name];
+        }
+    }
+    
+    return namesWithPref;
+}
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    NSMutableDictionary *iceCreamNameCounts = [@{} mutableCopy];
+    
+    for (NSString *iceCreamFlavor in [iceCreamByName allValues]) {
+        
+        if (!iceCreamNameCounts[iceCreamFlavor]) {
+            
+            iceCreamNameCounts[iceCreamFlavor] = @([[self namesForIceCream:iceCreamFlavor] count]);
+        }
+    }
+    
+    return iceCreamNameCounts;
+}
 
 @end
